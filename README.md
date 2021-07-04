8b3c4d8eb61de128d71696bf9b3e6d4a9f0c2c0a
AlejandroCoder <alejandropoupardh@gmail.com>
2021-07-04 14:22:52 -0700

    create files

---
+++ README.md
@@ -0,0 +1,6 @@
+"Hello Git and GitHub" 
+
+Rules
+#1 Set the main goal
+
+#2 Set the steps
---
+++ scripts.js
@@ -0,0 +1,6 @@
+
+let addAdj = () => {
+
+    document.getElementById("main").innerHTML = "This is the main parragraph!";
+
+}
